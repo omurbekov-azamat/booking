4e60601768b4e2009b5cec597fcf4ed9ec7ca440
Artem Markelov <artem77timoha77@gmail.com>
2023-04-11 16:44:02 +0600

    Сделал модель User, прописал его поля, указал обязательные поля и нет и валидация для поля email, создал файл type.d.ts прописал типы для модели User

---
+++ backend/types.d.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  phoneNumber: string;
+  status?: string;
+  cashback?: string;
+  token: string;
+}
