3a44e0b1a0803ec7457d8b9b7f2ed4645a95a7cc
Artem Markelov <artem77timoha77@gmail.com>
2023-04-11 20:36:59 +0600

    #20 - Сделал post запрос для проверки сессии .

--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -25,4 +25,27 @@ usersRouter.post('/', async (req, res, next) => {
   }
 });
 
+usersRouter.post('/sessions', async (req, res, next) => {
+  const user = await User.findOne({ username: req.body.username });
+
+  if (!user) {
+    return res.status(400).send({ error: 'Username or password incorrect' });
+  }
+
+  const isMatch = await user.checkPassword(req.body.password);
+
+  if (!isMatch) {
+    return res.status(400).send({ error: 'Username or password incorrect' });
+  }
+
+  try {
+    user.generateToken();
+    await user.save();
+
+    return res.send({ message: 'Username and password correct!', user });
+  } catch (e) {
+    return next(e);
+  }
+});
+
 export default usersRouter;
