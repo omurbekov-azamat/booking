0f42ab580a6e90af0d396f45ed6eb1f5587ec548
Nurdamir Akmataliev <nur140214@gmail.com>
2023-04-12 17:08:35 +0600

    Добавил изменения prettier в файлы: 18n.ts, App.tsx, index.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 function App() {
-
   const { t, i18n } = useTranslation();
 
-  const changeLanguage = (language:string) => {
+  const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
   };
 
-
   return (
     <div>
-      <button onClick={() => changeLanguage("en")}>EN</button>
-      <button onClick={() => changeLanguage("ru")}>RU</button>
-      <div>{t("text")}</div>
+      <button onClick={() => changeLanguage('en')}>EN</button>
+      <button onClick={() => changeLanguage('ru')}>RU</button>
+      <div>{t('text')}</div>
     </div>
   );
 }
