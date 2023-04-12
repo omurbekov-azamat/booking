9af18080a879112764199c03d1517ff6f58febf7
Artem Markelov <artem77timoha77@gmail.com>
2023-04-12 15:35:28 +0600

    #25 - Создал компонент UI AppToolbar.tsx . Сделал базовую разметку для header .

---
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useAppSelector } from '../../../app/hooks';
+import { AppBar, Box, Grid, styled, Toolbar, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const AppToolbar = () => {
+  // const user = useAppSelector(selectUser);
+
+  const Link = styled(NavLink)({
+    color: 'inherit',
+    textDecoration: 'none',
+    '&:hover': {
+      color: 'inherit',
+    },
+  });
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Typography variant="h6" component="div">
+                <Link to="/">Booking</Link>
+              </Typography>
+              {/*<Grid item>{user ? <UserMenu user={user} /> : <AnonymousMenu />}</Grid>*/}
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+};
+
+export default AppToolbar;
