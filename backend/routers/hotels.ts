195af77a0a495e052ddad1378e3762c759019c2d
Kwon Valerii <wellko24@gmail.com>
2023-04-13 14:45:58 +0600

    #29 добавил роут получение одного отеля по id

--- backend/routers/hotels.ts
+++ backend/routers/hotels.ts
@@ -41,4 +41,13 @@ hotelsRouter.get('/', async (req, res) => {
   }
 });
 
+hotelsRouter.get('/:id', async (req, res) => {
+  try {
+    const hotelsRes = await Hotel.find({ _id: req.params.id });
+    return res.send(hotelsRes);
+  } catch {
+    return res.sendStatus(500);
+  }
+});
+
 export default hotelsRouter;
