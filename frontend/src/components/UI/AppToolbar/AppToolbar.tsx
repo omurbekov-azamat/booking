105917c0794cb4af5f48e6599099a27bb32c9ad3
Edil Biktimirov <idyabik@gmail.com>
2023-04-14 10:56:42 +0600

    #44 - Оформил кнопки переключения языка
    Добавил russiaIcon.png, usaIcon.png

--- frontend/src/components/UI/AppToolbar/AppToolbar.tsx
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useAppSelector } from '../../../app/hooks';
-import { AppBar, Box, Grid, styled, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Grid, IconButton, styled, Toolbar, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import { selectUser } from '../../../features/users/usersSlice';
 import UserMenu from './UserMenu';
 import AnonymousMenu from './AnonymousMenu';
 import { useTranslation } from 'react-i18next';
+import RUIcon from '../../../assets/images/russiaIcon.png';
+import USAIcon from '../../../assets/images/usaIcon.png';
 
 const AppToolbar = () => {
   const user = useAppSelector(selectUser);
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
@@ -33,9 +35,12 @@ const AppToolbar = () => {
                 <Link to="/">Booking</Link>
               </Typography>
               <Grid item>
-                <button onClick={() => changeLanguage('en')}>EN</button>
-                <button onClick={() => changeLanguage('ru')}>RU</button>
-                <div>{t('text')}</div>
+                <IconButton style={{ width: 35, height: 35, padding: 0 }} onClick={() => changeLanguage('ru')}>
+                  <img src={RUIcon} alt="ru language" style={{ width: '100%', height: 'auto' }} />
+                </IconButton>
+                <IconButton style={{ width: 35, height: 35, padding: 0 }} onClick={() => changeLanguage('en')}>
+                  <img src={USAIcon} alt="eng language" style={{ width: '100%', height: 'auto' }} />
+                </IconButton>
               </Grid>
               <Grid item>{user ? <UserMenu user={user} /> : <AnonymousMenu />}</Grid>
             </Grid>
