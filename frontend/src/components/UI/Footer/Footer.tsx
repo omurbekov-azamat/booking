8842451f3df6ca3543a37b78281ed2fa35ce6f96
Kwon Valerii <wellko24@gmail.com>
2023-04-18 19:23:12 +0600

    #56 заполнил контентом футер с поддержкой мультиязычности

--- frontend/src/components/UI/Footer/Footer.tsx
+++ frontend/src/components/UI/Footer/Footer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { AppBar, Box, Grid, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Container, Grid, Link, Toolbar, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
   const { t } = useTranslation();
@@ -10,11 +14,41 @@ const Footer = () => {
       <Box sx={{ flexGrow: 1, mt: 1 }}>
         <AppBar position="static">
           <Toolbar>
-            <Grid container justifyContent="space-between" alignItems="center">
-              <Typography variant="h6" component="div">
-                {t('footerText')}
-              </Typography>
-            </Grid>
+            <Container>
+              <Grid container justifyContent="space-between" alignItems="center">
+                <Grid container item direction="column" xs={12} md={3}>
+                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                    +996 558 389288 <WhatsAppIcon />
+                  </Typography>
+                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                    +996 556 829978
+                    <CallIcon />
+                  </Typography>
+                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                    +996 505 000024
+                    <CallIcon />
+                  </Typography>
+                </Grid>
+                <Grid container item xs={12} md={3}>
+                  <Link href="http://www.eventm.kg/" style={{ margin: 'auto' }}>
+                    <img style={{ maxWidth: '250px' }} src="/logo.png" />
+                  </Link>
+                </Grid>
+                <Grid container item direction="column" xs={12} md={5}>
+                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                    <EmailIcon /> conference@eventm.kg
+                  </Typography>
+                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                    <Link href="https://www.instagram.com/eventm_agency" color="#FFF" underline="none">
+                      <InstagramIcon /> Eventm_agency
+                    </Link>
+                  </Typography>
+                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                    {t('footerAddress')}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Container>
           </Toolbar>
         </AppBar>
       </Box>
