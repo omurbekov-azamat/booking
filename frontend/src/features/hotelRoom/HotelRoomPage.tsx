00a31c4b350d8a44a324355d1ce920cce0f02a86
Artem Markelov <artem77timoha77@gmail.com>
2023-04-21 00:53:40 +0600

    #54 - создал разметку компонента, прописал условные данные о комнате отеля.

--- frontend/src/features/hotelRoom/HotelRoomPage.tsx
+++ frontend/src/features/hotelRoom/HotelRoomPage.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import { Grid } from '@mui/material';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
 
 const HotelRoomPage = () => {
-  return <></>;
+  const services = ['1', '2', '3'];
+  return (
+    <>
+      <Card sx={{ mt: 5 }}>
+        <CardContent>
+          <Typography variant="h4" component="p" textAlign={'center'}>
+            {'Название отеля'}
+          </Typography>
+          <Typography>{'Количество комнат:'}</Typography>
+          <Typography gutterBottom component="p">
+            {'Стоимость:'}
+          </Typography>
+
+          <Typography gutterBottom component="p">
+            {'Описание:'}
+          </Typography>
+
+          <Grid container xl>
+            {services.map((service, index) => {
+              return (
+                <Grid container gap={1} item key={index} alignSelf={'center'}>
+                  <Grid item>
+                    <TaskAltIcon />
+                  </Grid>
+                  <Grid item sx={{ fontSize: 17 }}>
+                    {service}
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default HotelRoomPage;
