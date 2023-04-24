ca5d0c911f7a71d07ba4f6b2c7dfa5f69d0dc7bf
Artem Markelov <artem77timoha77@gmail.com>
2023-04-25 01:06:56 +0600

    #64 - сделал patch запрос в apartmentThunks.ts

--- frontend/src/features/Apartment/apartmentThunks.ts
+++ frontend/src/features/Apartment/apartmentThunks.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import {Apartment, ApartmentMutation, Hotel, ValidationError} from '../../types';
+import { Apartment, ApartmentMutation, ValidationError } from '../../types';
 import { RootState } from '../../app/store';
 import axiosApi from '../../axiosApi';
 import { isAxiosError } from 'axios';
@@ -62,5 +62,40 @@ export const fetchOneApartment = createAsyncThunk<Apartment, string>('hotels/fet
     return response.data;
   } catch {
     throw new Error();
+  }
+});
+
+export const editApartment = createAsyncThunk<
+  void,
+  ApartmentMutation,
+  {
+    state: RootState;
+    rejectValue: ValidationError;
+  }
+>('apartments/editApartment', async (apartment, { getState, rejectWithValue }) => {
+  try {
+    const user = getState().users.user;
+
+    if (user) {
+      const formData = new FormData();
+      const keys = Object.keys(apartment) as (keyof ApartmentMutation)[];
+
+      keys.forEach((key) => {
+        const value = apartment[key];
+        if (value !== null) {
+          if ((key as string) === 'location') {
+            formData.append(key, JSON.stringify(value));
+          } else {
+            formData.append(key, value as string | Blob);
+          }
+        }
+      });
+      await axiosApi.patch('/apartments', formData);
+    }
+  } catch (e) {
+    if (isAxiosError(e) && e.response && e.response.status === 400) {
+      return rejectWithValue(e.response.data as ValidationError);
+    }
+    throw e;
   }
 });
