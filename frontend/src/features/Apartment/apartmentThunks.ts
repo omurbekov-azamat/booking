d4bb0ab520366f725fbda402fc47fb77f8a658e8
Artem Markelov <artem77timoha77@gmail.com>
2023-04-24 23:22:15 +0600

    #64 - сделал post запрос в apartmentThunks.ts

--- frontend/src/features/Apartment/apartmentThunks.ts
+++ frontend/src/features/Apartment/apartmentThunks.ts
@@ -1,36 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { HotelMutation, ValidationError } from '../../types';
+import { ApartmentMutation, ValidationError } from '../../types';
 import { RootState } from '../../app/store';
 import axiosApi from '../../axiosApi';
-import { isAxiosError } from 'axios/index';
+import { isAxiosError } from 'axios';
 
-export const createApartment = createAsyncThunk<void, HotelMutation, { state: RootState; rejectValue: ValidationError }>(
-  'apartment/createApartment',
-  async (hotel, { getState, rejectWithValue }) => {
-    try {
-      const user = getState().users.user;
+export const createApartment = createAsyncThunk<
+  void,
+  ApartmentMutation,
+  {
+    state: RootState;
+    rejectValue: ValidationError;
+  }
+>('apartment/createApartment', async (apartment, { getState, rejectWithValue }) => {
+  try {
+    const user = getState().users.user;
 
-      if (user) {
-        const formData = new FormData();
-        const keys = Object.keys(hotel) as (keyof HotelMutation)[];
+    if (user) {
+      const formData = new FormData();
+      const keys = Object.keys(apartment) as (keyof ApartmentMutation)[];
 
-        keys.forEach((key) => {
-          const value = hotel[key];
-          if (value !== null) {
-            if (key === 'location') {
-              formData.append(key, JSON.stringify(value));
-            } else {
-              formData.append(key, value as string | Blob);
-            }
+      keys.forEach((key) => {
+        const value = apartment[key];
+        if (value !== null) {
+          if ((key as string) === 'location') {
+            formData.append(key, JSON.stringify(value));
+          } else {
+            formData.append(key, value as string | Blob);
           }
-        });
-        await axiosApi.post('/hotels', formData);
-      }
-    } catch (e) {
-      if (isAxiosError(e) && e.response && e.response.status === 400) {
-        return rejectWithValue(e.response.data as ValidationError);
-      }
-      throw e;
+        }
+      });
+      await axiosApi.post('/apartments', formData);
     }
-  },
-);
+  } catch (e) {
+    if (isAxiosError(e) && e.response && e.response.status === 400) {
+      return rejectWithValue(e.response.data as ValidationError);
+    }
+    throw e;
+  }
+});
