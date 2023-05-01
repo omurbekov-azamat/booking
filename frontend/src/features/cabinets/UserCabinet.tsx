15f2c735746f23df16df04fe0d1512af89b36be5
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-01 17:51:32 +0600

    #84 - создал компонент Orders.tsx, для отображения всех заказов, данный компонент вызывается в UserCabinet.tsx;

--- frontend/src/features/cabinets/UserCabinet.tsx
+++ frontend/src/features/cabinets/UserCabinet.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import Orders from '../orders/components/Orders';
 
 const UserCabinet = () => {
-  const { t } = useTranslation();
   return (
     <>
-      <Typography component="div" variant="h5">
-        {t('myOrders')}
-      </Typography>
+      <Orders />
     </>
   );
 };
