23dc40eb583d21ca6eaea36afa63f531b7fac1ea
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-02 17:50:30 +0600

    #87 - добавил builders для сохраненя состояния запроса и данные админов,

--- frontend/src/features/cabinets/DirectorCabinet.tsx
+++ frontend/src/features/cabinets/DirectorCabinet.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
-import { useAppDispatch } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getAdmins } from '../users/usersThunks';
+import { selectAdmins, selectGetAdminsLoading } from '../users/usersSlice';
+import { Typography } from '@mui/material';
 
 const DirectorCabinet = () => {
   const dispatch = useAppDispatch();
+  const loading = useAppSelector(selectGetAdminsLoading);
+  const admins = useAppSelector(selectAdmins);
 
   useEffect(() => {
     dispatch(getAdmins());
   }, [dispatch]);
 
-  return <div>director cabinet</div>;
+  return (
+    <>
+      {loading && <Typography>loading...</Typography>}
+      <div> director cabinet</div>
+    </>
+  );
 };
 
 export default DirectorCabinet;
