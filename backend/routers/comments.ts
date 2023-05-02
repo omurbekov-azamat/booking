39f2dd91390a6314bdc9a697bfa323f95a834dc7
Edil Biktimirov <idyabik@gmail.com>
2023-05-02 17:54:38 +0600

    #99 - Добавил роут comments/patch

--- backend/routers/comments.ts
+++ backend/routers/comments.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import Comment from '../models/Comments';
-import { RequestWithUser } from '../middleware/auth';
+import auth, { RequestWithUser } from '../middleware/auth';
 import mongoose from 'mongoose';
 
 const commentsRouter = express.Router();
@@ -29,6 +29,29 @@ commentsRouter.post('/', async (req, res, next) => {
 
     await newComment.save();
     return res.send({ message: 'Created successfully' });
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(error);
+    }
+    return next(error);
+  }
+});
+
+commentsRouter.patch('/:id', auth, async (req, res, next) => {
+  try {
+    const user = (req as RequestWithUser).user;
+
+    const comment = await Comment.findOneAndUpdate(
+      { _id: req.params.id, author: user._id },
+      { text: req.body.text },
+      { new: true, runValidators: true },
+    );
+
+    if (!comment) {
+      return res.status(403).send({ message: 'Forbidden' });
+    }
+
+    return res.status(200).send({ message: 'Comment updated successfully' });
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
       return res.status(400).send(error);
