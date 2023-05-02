27492ca8a108c910d7f853ea20e1d021b84db8c7
Edil Biktimirov <idyabik@gmail.com>
2023-05-02 17:15:42 +0600

    #99 - Добавил модель Comments

---
+++ backend/models/Comments.ts
@@ -0,0 +1,37 @@
+import mongoose, { Types, Schema } from 'mongoose';
+import User from './User';
+import { IComment } from '../types';
+import Hotel from './Hotel';
+
+const CommentSchema = new Schema<IComment>({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => await User.findById(value),
+      message: 'User not found!',
+    },
+  },
+  hotel: {
+    type: Schema.Types.ObjectId,
+    ref: 'Hotel',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => await Hotel.findById(value),
+      message: 'Hotel not found!',
+    },
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Comment = mongoose.model<IComment>('Comment', CommentSchema);
+
+export default Comment;
