ee24d2ae4e5b3bf89528142ffad1e96d738cb2c2
Edil Biktimirov <idyabik@gmail.com>
2023-05-03 09:51:26 +0600

    #103 - Добавил санк на /post

--- frontend/src/features/comments/commentsThunks.ts
+++ frontend/src/features/comments/commentsThunks.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { Comment } from '../../types';
+import { Comment, CommentMutation, ValidationError } from '../../types';
 import axiosApi from '../../axiosApi';
+import { RootState } from '../../app/store';
+import { isAxiosError } from 'axios';
 
 export const fetchComments = createAsyncThunk<Comment[], string>(
   'comments/fetchAll',
@@ -13,3 +15,22 @@ export const fetchComments = createAsyncThunk<Comment[], string>(
     }
   },
 );
+
+export const createComment = createAsyncThunk<
+  void,
+  CommentMutation,
+  { state: RootState; rejectValue: ValidationError }
+>('comments/createComment', async (comment, { getState, rejectWithValue }) => {
+  try {
+    const user = getState().users.user;
+
+    if (user) {
+      await axiosApi.post('/comments', comment);
+    }
+  } catch (e) {
+    if (isAxiosError(e) && e.response && e.response.status === 400) {
+      return rejectWithValue(e.response.data as ValidationError);
+    }
+    throw e;
+  }
+});
