44971339603e55eaf578d537f567e52c3404848a
Edil Biktimirov <idyabik@gmail.com>
2023-05-03 16:11:04 +0600

    #103 - Добавил функцию deleteComment в CommentMessage.tsx Добавил пропсы onDeleteBtnClick, onEditBtnClick в CommentMessage.tsx

--- frontend/src/features/comments/Comments.tsx
+++ frontend/src/features/comments/Comments.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { selectComments } from './commentsSlice';
-import { fetchComments } from './commentsThunks';
-import { useParams } from 'react-router-dom';
+import { fetchComments, removeComment } from './commentsThunks';
+import { useNavigate, useParams } from 'react-router-dom';
 import CommentMessage from './components/CommentMessage';
 
 const Comments = () => {
   const dispatch = useAppDispatch();
   const { id } = useParams() as { id: string };
+  const navigate = useNavigate();
   const comments = useAppSelector(selectComments);
+
+  const deleteComment = async (id: string) => {
+    await dispatch(removeComment(id));
+    await dispatch(fetchComments(id));
+  };
 
   useEffect(() => {
     dispatch(fetchComments(id));
@@ -17,7 +23,14 @@ const Comments = () => {
   return (
     <>
       {comments.map((comment) => {
-        return <CommentMessage comment={comment} key={comment._id} />;
+        return (
+          <CommentMessage
+            comment={comment}
+            onDeleteBtnClick={() => deleteComment(id)}
+            onEditBtnClick={() => navigate('/hotels/' + id + '/edit-comment')}
+            key={comment._id}
+          />
+        );
       })}
     </>
   );
--- frontend/src/features/comments/components/CommentMessage.tsx
+++ frontend/src/features/comments/components/CommentMessage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Grid } from '@mui/material';
+import React, { MouseEventHandler } from 'react';
+import { Button, Grid, Typography } from '@mui/material';
 import { Comment } from '../../../types';
 import dayjs from 'dayjs';
 import { useAppSelector } from '../../../app/hooks';
@@ -7,20 +7,34 @@ import { selectUser } from '../../users/usersSlice';
 
 interface Props {
   comment: Comment;
+  onDeleteBtnClick: MouseEventHandler;
+  onEditBtnClick: MouseEventHandler;
 }
 
-const CommentMessage: React.FC<Props> = ({ comment }) => {
+const CommentMessage: React.FC<Props> = ({ comment, onDeleteBtnClick, onEditBtnClick }) => {
   const user = useAppSelector(selectUser);
-  const deleteBtn = <Button>Delete</Button>;
-  const editBtn = <Button>Edit</Button>;
+  const deleteBtn = (
+    <Button onClick={onDeleteBtnClick} variant={'outlined'} color={'error'}>
+      Delete
+    </Button>
+  );
+  const editBtn = (
+    <Button onClick={onEditBtnClick} variant={'outlined'} color={'info'} sx={{ my: 1 }}>
+      Edit
+    </Button>
+  );
 
   return (
-    <Grid container>
-      <Grid item>{comment.author} </Grid>
-      <Grid item>{comment.text} </Grid>
+    <Grid container flexDirection={'column'} border={1} my={2} p={2}>
+      <Grid item>{'From: ' + comment.author} </Grid>
+      <Grid item>
+        <Typography variant={'h6'} component={'p'}>
+          {comment.text}
+        </Typography>
+      </Grid>
       <Grid item>{dayjs(comment.createdAt).format('DD-MM-YYYY HH:mm')} </Grid>
-      {user?.role === 'admin' || user?.role === 'director' || user?._id === comment.author ? deleteBtn : null}
       {user?._id === comment.author ? editBtn : null}
+      {user?.role === 'admin' || user?.role === 'director' || user?._id === comment.author ? deleteBtn : null}
     </Grid>
   );
 };
