5f65075c68e858b949cf450ae6073b8b7227f256
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-05 11:45:14 +0600

    #104 - в UserCabinet.tsx добавил list menu, с выбором my orders и favorites

--- frontend/src/features/cabinets/UserCabinet.tsx
+++ frontend/src/features/cabinets/UserCabinet.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box, Card, CardContent, Grid, List, Typography } from '@mui/material';
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import ListItemButton from '@mui/material/ListItemButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import Orders from '../orders/components/Orders';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 const UserCabinet = () => {
+  const { t } = useTranslation();
+  const [state, setState] = React.useState({
+    orders: true,
+    favorites: false,
+  });
+
+  const handleClickOrders = () => {
+    setState((prev) => ({ ...prev, orders: true, favorites: false }));
+  };
+
+  const handleClickFavorites = () => {
+    setState((prev) => ({ ...prev, orders: false, favorites: true }));
+  };
+
   return (
-    <>
-      <Orders />
-    </>
+    <Box mt={3}>
+      <Card>
+        <CardContent>
+          <Grid container flexDirection="row" spacing={2} alignItems="self-start">
+            <Grid item xs>
+              <List
+                sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+              >
+                <ListItemButton onClick={handleClickOrders}>
+                  <ListItemIcon>
+                    <MapsHomeWorkIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t('myOrders')} />
+                </ListItemButton>
+                <ListItemButton onClick={handleClickFavorites}>
+                  <ListItemIcon>
+                    <FavoriteIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t('myFavorites')} />
+                </ListItemButton>
+              </List>
+            </Grid>
+            <Grid item xs>
+              <Box overflow="auto" height="300px">
+                {state.orders && <Orders />}
+                {state.favorites && <Typography>Here will be favorites</Typography>}
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
--- frontend/src/features/orders/components/Orders.tsx
+++ frontend/src/features/orders/components/Orders.tsx
@@ -2,28 +2,23 @@ import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { selectOrders } from '../ordersSlice';
 import { getOrders } from '../ordersThunks';
-import { Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import { Grid } from '@mui/material';
 import OrderCard from './OrderCard';
 
 const Orders = () => {
   const dispatch = useAppDispatch();
   const orders = useAppSelector(selectOrders);
-  const { t } = useTranslation();
 
   useEffect(() => {
     dispatch(getOrders());
   }, [dispatch]);
 
   return (
-    <>
-      <Typography component="div" variant="h5" textAlign="center" mt={3}>
-        {t('myOrders')}
-      </Typography>
+    <Grid container spacing={2}>
       {orders.map((order) => (
         <OrderCard key={order._id} prop={order} />
       ))}
-    </>
+    </Grid>
   );
 };
 
