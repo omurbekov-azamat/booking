16f2c67a6273735116035006ed6f39d0cbcb7d01
Artem Markelov <artem77timoha77@gmail.com>
2023-05-05 19:46:24 +0600

    #107 - Стилизовал кнопку чтоб была как логотип вотсАпп

--- frontend/src/components/UI/WhatsAppButton/WhatsAppButton.tsx
+++ frontend/src/components/UI/WhatsAppButton/WhatsAppButton.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 const WhatsAppButton = () => {
+  const MyButton = styled(Button)({
+    position: 'fixed',
+    bottom: 40,
+    right: 40,
+    width: 50,
+    height: 60,
+    padding: 0,
+    borderRadius: '50%',
+    backgroundColor: '#25d366',
+    '&:hover': {
+      backgroundColor: '#128c7e',
+    },
+    zIndex: 9999,
+  });
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#ffffff',
+      },
+    },
+  });
   return (
     <>
-      <Button>
-        <WhatsAppIcon sx={{ color: '#ffffff' }} />
-      </Button>
+      <ThemeProvider theme={theme}>
+        <MyButton>
+          <WhatsAppIcon sx={{ color: '#ffffff' }} />
+        </MyButton>
+      </ThemeProvider>
     </>
   );
 };
