afdb1136ff26883be8558951bcaba729a2acc2c0
Artem Markelov <artem77timoha77@gmail.com>
2023-05-09 00:38:14 +0600

    #111 - Сделал основу (верстку) для будущего вывода карточек , в двух разделах Премиум и Бизнес.

--- frontend/src/features/hotels/components/VipBlockHotels.tsx
+++ frontend/src/features/hotels/components/VipBlockHotels.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Box, Grid } from '@mui/material';
 
 const VipBlockHotels = () => {
-  return <></>;
+  return (
+    <>
+      <Box
+        border={3}
+        borderColor="black"
+        borderRadius={5}
+        p={2}
+        mt={2}
+        sx={{ position: 'relative', minHeight: '300px' }}
+      >
+        <Box textAlign="center" fontWeight="bold" mt={2}>
+          Best Hotels
+        </Box>
+        <Grid container spacing={2} alignItems="stretch" sx={{ marginTop: '10px' }}></Grid>
+      </Box>
+
+      <Box
+        border={3}
+        borderColor="gray"
+        borderRadius={5}
+        p={2}
+        mt={2}
+        sx={{ position: 'relative', minHeight: '300px' }}
+      >
+        <Box textAlign="center" fontWeight="bold" mt={2}>
+          Recommended
+        </Box>
+        <Grid container spacing={2} alignItems="stretch" sx={{ marginTop: '10px' }}></Grid>
+      </Box>
+    </>
+  );
 };
 
 export default VipBlockHotels;
