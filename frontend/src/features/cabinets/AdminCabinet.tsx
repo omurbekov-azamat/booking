ecc2670dff447af5b96dcb36618cabcdc55b13a5
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-10 16:44:34 +0600

    #120 - изменил страницу кабинет админа, добавил menu list

--- frontend/src/features/cabinets/AdminCabinet.tsx
+++ frontend/src/features/cabinets/AdminCabinet.tsx
@@ -1,87 +1,115 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Container, FormControlLabel, Grid, Radio, RadioGroup, Typography } from '@mui/material';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { selectHotels } from '../hotels/hotelsSlice';
-import HotelsCard from '../hotels/components/HotelsCard';
-import { fetchHotels } from '../hotels/hotelsThunks';
-import { selectUser } from '../users/usersSlice';
-import { selectAdminMyOrders } from '../orders/ordersSlice';
-import { getForAdminHisOrders } from '../orders/ordersThunks';
-import OrderCard from '../orders/components/OrderCard';
+import React, { useEffect } from 'react';
+import { Box, Card, Grid, List, ListItemButton } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+import CardContent from '@mui/material/CardContent';
+import HomeIcon from '@mui/icons-material/Home';
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { selectUser } from '../users/usersSlice';
+import { fetchHotels } from '../hotels/hotelsThunks';
+import { getForAdminHisOrders } from '../orders/ordersThunks';
+import { selectHotels } from '../hotels/hotelsSlice';
+import { selectAdminMyOrders } from '../orders/ordersSlice';
+import OrderCard from '../orders/components/OrderCard';
+import HotelsCard from '../hotels/components/HotelsCard';
 
 const AdminCabinet = () => {
   const user = useAppSelector(selectUser);
-  const hotelsState = useAppSelector(selectHotels);
-  const orders = useAppSelector(selectAdminMyOrders);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
-  const navigate = useNavigate();
+  const hotelsState = useAppSelector(selectHotels);
+  const orders = useAppSelector(selectAdminMyOrders);
 
-  const [state, setState] = useState({
+  const [state, setState] = React.useState({
+    myInfo: true,
     myHotels: false,
     orders: false,
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.checked,
-      [e.target.name === 'myHotels' ? 'orders' : 'myHotels']: false,
-    });
-  };
-
   useEffect(() => {
     if (user) {
-      dispatch(fetchHotels(user._id));
-      dispatch(getForAdminHisOrders(user._id));
+      if (state.myHotels) {
+        dispatch(fetchHotels(user._id));
+      }
+      if (state.orders) {
+        dispatch(getForAdminHisOrders(user._id));
+      }
     }
-  }, [dispatch, user]);
+  }, [dispatch, user, state.myHotels, state.orders]);
+
+  const handleClickMyInfo = () => {
+    setState((prev) => ({ ...prev, orders: false, myHotels: false, myInfo: true }));
+  };
+
+  const handleClickMyHotels = () => {
+    setState((prev) => ({ ...prev, orders: false, myHotels: true, myInfo: false }));
+  };
+
+  const handleClickOrders = () => {
+    setState((prev) => ({ ...prev, orders: true, myHotels: false, myInfo: false }));
+  };
 
   return (
-    <>
-      <Container>
-        <Typography variant="h4" component="p" textAlign={'center'}>
-          {t('adminCabinet')}
-        </Typography>
-        <Grid container justifyContent={'end'}>
-          <Button variant="contained" color="success" onClick={() => navigate('/addHotel')}>
-            {t('createHotel')}
-          </Button>
-        </Grid>
-        <RadioGroup>
-          <FormControlLabel
-            control={<Radio checked={state.myHotels} onChange={handleChange} name="myHotels" />}
-            label={t('myHotels')}
-          />
-          <FormControlLabel
-            control={<Radio checked={state.orders} onChange={handleChange} name="orders" />}
-            label={t('myOrders')}
-          />
-        </RadioGroup>
-      </Container>
-
-      {state.myHotels && (
-        <>
-          <Grid container spacing={2} alignItems="stretch" sx={{ marginTop: '10px' }}>
-            {hotelsState.map((el) => (
-              <Grid item xs={12} sm={6} lg={4} key={Math.random()} alignItems="stretch">
-                <HotelsCard hotel={el} />
-              </Grid>
-            ))}
+    <Box mt={3}>
+      <Card sx={{ minHeight: '600px' }}>
+        <CardContent>
+          <Grid container flexDirection="row" spacing={2} alignItems="self-start">
+            <Grid item xs={12} sm={6} md={3}>
+              <List
+                sx={{
+                  width: '100%',
+                  maxWidth: 360,
+                  bgcolor: 'background.paper',
+                  border: '2px solid #c5c5c5',
+                }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+              >
+                <ListItemButton onClick={handleClickMyInfo}>
+                  <ListItemIcon>
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t('myInfo')} />
+                </ListItemButton>
+                <ListItemButton onClick={handleClickOrders}>
+                  <ListItemIcon>
+                    <MapsHomeWorkIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t('myOrders')} />
+                </ListItemButton>
+                <ListItemButton onClick={handleClickMyHotels}>
+                  <ListItemIcon>
+                    <FavoriteIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t('myHotels')} />
+                </ListItemButton>
+              </List>
+            </Grid>
+            <Grid item xs>
+              {state.orders && (
+                <Grid container spacing={2}>
+                  {orders.map((item) => {
+                    return <OrderCard prop={item} key={item._id} />;
+                  })}
+                </Grid>
+              )}
+              {state.myHotels && (
+                <Grid container spacing={2} alignItems="stretch">
+                  {hotelsState.map((el) => (
+                    <Grid item key={el._id}>
+                      <HotelsCard hotel={el} />
+                    </Grid>
+                  ))}
+                </Grid>
+              )}
+            </Grid>
           </Grid>
-        </>
-      )}
-
-      {state.orders && (
-        <>
-          {orders.map((item) => {
-            return <OrderCard prop={item} key={item._id} />;
-          })}
-        </>
-      )}
-    </>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
