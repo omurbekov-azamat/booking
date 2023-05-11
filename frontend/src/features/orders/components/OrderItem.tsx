abec8a3fc1e19539cff0309134deeadc91bd6915
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-11 11:22:49 +0600

    #121 - добавил перевод данных в компонентах OrderItem.tsx и OrderItems.tsx

--- frontend/src/features/orders/components/OrderItem.tsx
+++ frontend/src/features/orders/components/OrderItem.tsx
@@ -6,29 +6,37 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useTranslation } from 'react-i18next';
 
 interface props {
   prop: Order;
 }
 
 const OrderItem: React.FC<props> = ({ prop }) => {
+  const { t } = useTranslation();
   return (
     <Accordion>
       <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
         <Grid container justifyContent="space-between">
-          <Grid item>
-            <Typography variant="subtitle2">Заказ № {prop._id}</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle2">Дата создания: {dayjs(prop.createdAt).format('DD-MM-YYYY')}</Typography>
-          </Grid>
-          <Grid item>
+          <Grid item xs={12} sm={12} lg={6} xl={3}>
             <Typography variant="subtitle2">
-              Имя заказчика: {prop.userId.firstName} {prop.userId.lastName}
+              {t('order')} № {prop._id}
             </Typography>
           </Grid>
-          <Grid item>
-            <Typography variant="subtitle2">Telephone number: {prop.userId.phoneNumber}</Typography>
+          <Grid item xs={12} sm={12} lg={6} xl={3}>
+            <Typography variant="subtitle2">
+              {t('customerName')}: {prop.userId.firstName} {prop.userId.lastName}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} lg={6} xl={3}>
+            <Typography variant="subtitle2">
+              {t('creationDate')}: {dayjs(prop.createdAt).format('DD-MM-YYYY')}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} lg={6} xl={3}>
+            <Typography variant="subtitle2">
+              {t('telephoneNumber')}: {prop.userId.phoneNumber}
+            </Typography>
           </Grid>
         </Grid>
       </AccordionSummary>
--- frontend/src/features/orders/components/OrderItems.tsx
+++ frontend/src/features/orders/components/OrderItems.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import OrderItem from './OrderItem';
 import { Typography } from '@mui/material';
 import { Order } from '../../../types';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   ordersItems: Order[];
 }
 
 const OrderItems: React.FC<Props> = ({ ordersItems }) => {
+  const { t } = useTranslation();
   return (
     <>
       {ordersItems.length > 0 ? (
         ordersItems.map((item) => <OrderItem key={item._id} prop={item} />)
       ) : (
-        <Typography>There are no orders</Typography>
+        <Typography>{t('thereAreNoOrders')}</Typography>
       )}
     </>
   );
