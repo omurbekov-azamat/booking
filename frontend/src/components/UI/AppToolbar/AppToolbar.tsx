aa1c33a6de4e1cd42e07176543b9b5d77203e732
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-12 13:55:03 +0600

    #124 - изменил цвет хедера и футера, добавил максимальную ширину контэйнера, добавил ссылку ведущую на /hotels

--- frontend/src/components/UI/AppToolbar/AppToolbar.tsx
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAppSelector } from '../../../app/hooks';
-import { AppBar, Box, Grid, IconButton, styled, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Container, Grid, IconButton, styled, Toolbar, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import { selectUser } from '../../../features/users/usersSlice';
 import UserMenu from './UserMenu';
@@ -27,29 +27,40 @@ const AppToolbar = () => {
   });
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ flexGrow: 1 }} mb={1}>
+      <AppBar position="sticky" sx={{ top: 0, background: '#77dd77', py: 2 }}>
         <Toolbar>
-          <Grid container justifyContent="space-between" alignItems="center">
-            <Typography variant="h6" component="div">
-              <Link to="/">Booking</Link>
-            </Typography>
-            <Grid item>
-              <IconButton style={{ width: 35, height: 35, padding: 0 }} onClick={() => changeLanguage('ru')}>
-                <img src={RUIcon} alt="ru language" style={{ width: '100%', height: 'auto' }} />
-              </IconButton>
-              <IconButton style={{ width: 35, height: 35, padding: 0 }} onClick={() => changeLanguage('en')}>
-                <img src={USAIcon} alt="eng language" style={{ width: '100%', height: 'auto' }} />
-              </IconButton>
+          <Container maxWidth="xl">
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                <Grid container justifyContent="space-between">
+                  <Typography variant="h6" component="div">
+                    <Link to="/">Booking</Link>
+                  </Typography>
+                  <Typography variant="h6" component="div" mr={1}>
+                    <Link to="/hotels">Hotels</Link>
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={6} sm={6} md={2} lg={2} xl={2}>
+                <IconButton style={{ width: 35, height: 35, padding: 0 }} onClick={() => changeLanguage('ru')}>
+                  <img src={RUIcon} alt="ru language" style={{ width: '100%', height: 'auto' }} />
+                </IconButton>
+                <IconButton style={{ width: 35, height: 35, padding: 0 }} onClick={() => changeLanguage('en')}>
+                  <img src={USAIcon} alt="eng language" style={{ width: '100%', height: 'auto' }} />
+                </IconButton>
+              </Grid>
+              <Grid item xs={6} sm={6} md={3} lg={3} xl={2}>
+                <Typography variant="h6" display="inline-block">
+                  {t('currency')}
+                </Typography>
+                <CurrencySwitcher />
+              </Grid>
+              <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+                {user ? <UserMenu user={user} /> : <AnonymousMenu />}
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant="h6" display="inline-block">
-                {t('currency')}
-              </Typography>
-              <CurrencySwitcher />
-            </Grid>
-            <Grid item>{user ? <UserMenu user={user} /> : <AnonymousMenu />}</Grid>
-          </Grid>
+          </Container>
         </Toolbar>
       </AppBar>
     </Box>
--- frontend/src/components/UI/Footer/Footer.tsx
+++ frontend/src/components/UI/Footer/Footer.tsx
@@ -10,49 +10,47 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <Box sx={{ flexGrow: 1, mt: 1 }}>
-        <AppBar position="static">
-          <Toolbar sx={{ paddingY: '10px' }}>
-            <Container>
-              <Grid container justifyContent="space-between" alignItems="center">
-                <Grid container item direction="column" xs={12} md={3}>
-                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
-                    +996 558 389288 <WhatsAppIcon />
-                  </Typography>
-                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
-                    +996 556 829978
-                    <CallIcon />
-                  </Typography>
-                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
-                    +996 505 000024
-                    <CallIcon />
-                  </Typography>
-                </Grid>
-                <Grid container item xs={12} md={3}>
-                  <Link href="http://www.eventm.kg/" style={{ margin: 'auto' }}>
-                    <img style={{ maxWidth: '250px' }} src="/logo.png" alt="logo" />
-                  </Link>
-                </Grid>
-                <Grid container item direction="column" xs={12} md={5}>
-                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
-                    <EmailIcon /> conference@eventm.kg
-                  </Typography>
-                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
-                    <Link href="https://www.instagram.com/eventm_agency" color="#FFF" underline="none">
-                      <InstagramIcon /> Eventm_agency
-                    </Link>
-                  </Typography>
-                  <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
-                    {t('footerAddress')}
-                  </Typography>
-                </Grid>
+    <Box sx={{ flexGrow: 1, mt: 1 }}>
+      <AppBar position="static" sx={{ background: '#77dd77' }}>
+        <Toolbar sx={{ paddingY: '10px' }}>
+          <Container maxWidth="xl">
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid container item direction="column" xs={12} md={3}>
+                <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                  +996 558 389288 <WhatsAppIcon />
+                </Typography>
+                <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                  +996 556 829978
+                  <CallIcon />
+                </Typography>
+                <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                  +996 505 000024
+                  <CallIcon />
+                </Typography>
               </Grid>
-            </Container>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
+              <Grid container item xs={12} md={3}>
+                <Link href="http://www.eventm.kg/" style={{ margin: 'auto' }}>
+                  <img style={{ maxWidth: '250px' }} src="/logo.png" alt="logo" />
+                </Link>
+              </Grid>
+              <Grid container item direction="column" xs={12} md={5}>
+                <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                  <EmailIcon /> conference@eventm.kg
+                </Typography>
+                <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                  <Link href="https://www.instagram.com/eventm_agency" color="#FFF" underline="none">
+                    <InstagramIcon /> Eventm_agency
+                  </Link>
+                </Typography>
+                <Typography variant="body1" component="div" style={{ margin: 'auto' }}>
+                  {t('footerAddress')}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
