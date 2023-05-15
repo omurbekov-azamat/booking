ebd4e9e31d67a7619f7bf9531ee5a702e732c28d
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-15 20:27:42 +0600

    #131 - добавил спиннер при запросе на получение всех коментариев

--- frontend/src/features/comments/Comments.tsx
+++ frontend/src/features/comments/Comments.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { selectComments } from './commentsSlice';
+import { selectComments, selectLoadingFetchAllComments } from './commentsSlice';
 import { fetchComments, removeComment } from './commentsThunks';
 import { useNavigate, useParams } from 'react-router-dom';
 import CommentMessage from './components/CommentMessage';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const Comments = () => {
   const dispatch = useAppDispatch();
   const { id } = useParams() as { id: string };
   const navigate = useNavigate();
   const comments = useAppSelector(selectComments);
+  const loadingFetchAllComments = useAppSelector(selectLoadingFetchAllComments);
 
   const deleteComment = async (commentId: string, hotelId: string) => {
     await dispatch(removeComment(commentId));
@@ -22,6 +24,7 @@ const Comments = () => {
 
   return (
     <>
+      {loadingFetchAllComments && <Spinner />}
       {comments.map((comment) => {
         return (
           <CommentMessage
