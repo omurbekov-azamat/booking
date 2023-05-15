e0a7b66d616b03966834927ecd0837d48f846d17
Edil Biktimirov <idyabik@gmail.com>
2023-05-15 14:31:02 +0600

    #125 - Создал компонент ApartmentsCard.tsx, который принимает пропс apartment

---
+++ frontend/src/features/apartments/components/ApartmentsCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IApartment } from '../../../types';
+
+interface Props {
+  apartment: IApartment;
+}
+
+const ApartmentsCard: React.FC<Props> = ({ apartment }) => {
+  return <div></div>;
+};
+
+export default ApartmentsCard;
