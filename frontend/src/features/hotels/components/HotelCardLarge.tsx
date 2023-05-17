d149b8ce8c69e05900353284a572b4eb42172b7f
Edil Biktimirov <idyabik@gmail.com>
2023-05-17 16:28:46 +0600

    #136 - Добавил поддержку языка для города

--- frontend/src/features/hotels/components/HotelCardLarge.tsx
+++ frontend/src/features/hotels/components/HotelCardLarge.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Box, CardMedia, Grid, Link, Rating, Typography } from '@mui/material';
+import React, { MouseEventHandler } from 'react';
+import { Box, CardMedia, Checkbox, Grid, Link, Rating, Typography } from '@mui/material';
 import { Hotel } from '../../../types';
 import { apiURL } from '../../../constants';
 import { useTranslation } from 'react-i18next';
 import { Link as RouterLink } from 'react-router-dom';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
 
 interface Props {
   hotel: Hotel;
   commentAmount?: number;
+  onFavoriteIconClick?: MouseEventHandler;
 }
 
-const HotelCardLarge: React.FC<Props> = ({ hotel, commentAmount }) => {
+const HotelCardLarge: React.FC<Props> = ({ hotel, commentAmount, onFavoriteIconClick }) => {
   const cardImage = apiURL + '/' + hotel.image;
   const { t } = useTranslation();
 
   let statusStyle = {
-    border: '2px solid silver',
+    border: '4px solid silver',
     borderRadius: '6px',
     padding: '7px',
+    fontWeight: 'bold',
   };
+
+  if (hotel.status === 'premium') {
+    statusStyle = {
+      ...statusStyle,
+      border: '2px solid gold',
+    };
+  } else if (hotel.status === 'business') {
+    statusStyle = {
+      ...statusStyle,
+      border: '2px solid blue',
+    };
+  }
 
   let status = '';
 
@@ -32,18 +47,67 @@ const HotelCardLarge: React.FC<Props> = ({ hotel, commentAmount }) => {
     case 'standard':
       status = t('standard');
       break;
+    default:
+      break;
   }
 
-  if (hotel.status === 'premium') {
-    statusStyle = {
-      ...statusStyle,
-      border: '2px solid gold',
-    };
-  } else if (hotel.status === 'business') {
-    statusStyle = {
-      ...statusStyle,
-      border: '2px solid blue',
-    };
+  let city = '';
+
+  switch (hotel.city) {
+    case 'bishkek':
+      city = t('bishkek');
+      break;
+    case 'issykKul':
+      city = t('issykKul');
+      break;
+    case 'osh':
+      status = t('osh');
+      break;
+    case 'kara-balta':
+      status = t('kara-balta');
+      break;
+    case 'tokmok':
+      status = t('tokmok');
+      break;
+    case 'kant':
+      status = t('kant');
+      break;
+    case 'talas':
+      status = t('talas');
+      break;
+    case 'kara-suu':
+      status = t('kara-suu');
+      break;
+    case 'nookat':
+      status = t('nookat');
+      break;
+    case 'uzgen':
+      status = t('uzgen');
+      break;
+    case 'suzak':
+      status = t('suzak');
+      break;
+    case 'kara-kulja':
+      status = t('kara-kulja');
+      break;
+    case 'naryn':
+      status = t('naryn');
+      break;
+    case 'atbashi':
+      status = t('atbashi');
+      break;
+    case 'kochkor':
+      status = t('kochkor');
+      break;
+    case 'isfana':
+      status = t('isfana');
+      break;
+    case 'kyzyl-kiya':
+      status = t('kyzyl-kiya');
+      break;
+    case 'leylek':
+      status = t('leylek');
+      break;
   }
 
   return (
@@ -61,7 +125,9 @@ const HotelCardLarge: React.FC<Props> = ({ hotel, commentAmount }) => {
           <Grid container flexDirection="row" justifyContent="space-between">
             <Grid container gap={1} alignItems="center">
               <Grid item>
-                <Typography variant="h5">{hotel.name}</Typography>
+                <Typography variant="h5" fontWeight="bolder">
+                  {hotel.name}
+                </Typography>
               </Grid>
               <Grid item>
                 <Rating name="read-only" value={hotel.star} precision={0.5} readOnly size="small" />
@@ -70,24 +136,24 @@ const HotelCardLarge: React.FC<Props> = ({ hotel, commentAmount }) => {
           </Grid>
 
           <Grid item>
-            <Grid container gap={1} alignItems="center">
-              <Grid item>
-                <Typography variant="h6">{hotel.city}</Typography>
-              </Grid>
-
-              <Grid item>
-                <Typography color={'grey'}>{t('lowestPrice') + ' ' + hotel.lowestPrice.dollar + ' USD'}</Typography>
-              </Grid>
+            <Grid item>
+              <Typography variant="h6">{city}</Typography>
             </Grid>
-          </Grid>
-
-          <Grid item>
-            <Typography color={'grey'}>{t('founding') + ' ' + hotel.founding}</Typography>
+            <Grid item>
+              <Typography color={'grey'} fontSize={18}>
+                {t('founding') + ' ' + hotel.founding}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography color={'grey'} fontSize={18}>
+                {t('lowestPrice') + ' ' + hotel.lowestPrice.dollar + ' USD'}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
 
         <Grid item>
-          <Grid container flexDirection="column">
+          <Grid container flexDirection="column" alignItems="center">
             <Grid item>
               <Box style={statusStyle} textAlign={'center'}>
                 {status}
@@ -95,9 +161,22 @@ const HotelCardLarge: React.FC<Props> = ({ hotel, commentAmount }) => {
             </Grid>
 
             <Grid item>
-              <Link style={{ textDecoration: 'none' }} component={RouterLink} to="/login" variant="body2">
+              <Link
+                style={{ textDecoration: 'none', color: '#6b6b6b' }}
+                component={RouterLink}
+                to="/login"
+                variant="body2"
+              >
                 {t('comments') + ': ' + commentAmount}
               </Link>
+            </Grid>
+            <Grid item>
+              <Checkbox
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite />}
+                onClick={onFavoriteIconClick}
+                color="error"
+              />
             </Grid>
           </Grid>
         </Grid>
