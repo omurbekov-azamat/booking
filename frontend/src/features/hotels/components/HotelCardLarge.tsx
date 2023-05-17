d549141629d120414acba5fecccd71dd7035c73b
Edil Biktimirov <idyabik@gmail.com>
2023-05-17 13:36:04 +0600

    #136 - Добавил отображение картинки

--- frontend/src/features/hotels/components/HotelCardLarge.tsx
+++ frontend/src/features/hotels/components/HotelCardLarge.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { CardMedia, Grid } from '@mui/material';
+import { Hotel } from '../../../types';
+import { apiURL } from '../../../constants';
 
-const HotelCardLarge = () => {
-  return <div></div>;
+interface Props {
+  hotel: Hotel;
+}
+
+const HotelCardLarge: React.FC<Props> = ({ hotel }) => {
+  const cardImage = apiURL + '/' + hotel.image;
+
+  return (
+    <>
+      <Grid container style={{ border: '1px solid grey', padding: '15px', marginBottom: '10px' }}>
+        <Grid item style={{ maxWidth: '200px', maxHeight: '200px' }}>
+          <CardMedia component="img" width="100%" height="auto" image={cardImage} alt={hotel.name} />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default HotelCardLarge;
