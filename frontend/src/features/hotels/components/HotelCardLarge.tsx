0b3fe6fc8fa79ec046b3a041717fe77fa9e3a113
Edil Biktimirov <idyabik@gmail.com>
2023-05-17 14:02:37 +0600

    #136 - Вывел отображение названия отеля и звезд

--- frontend/src/features/hotels/components/HotelCardLarge.tsx
+++ frontend/src/features/hotels/components/HotelCardLarge.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { CardMedia, Grid } from '@mui/material';
+import React, { MouseEventHandler } from 'react';
+import { CardMedia, Grid, Rating, Typography } from '@mui/material';
 import { Hotel } from '../../../types';
 import { apiURL } from '../../../constants';
 
@@ -12,9 +12,22 @@ const HotelCardLarge: React.FC<Props> = ({ hotel }) => {
 
   return (
     <>
-      <Grid container style={{ border: '1px solid grey', padding: '15px', marginBottom: '10px' }}>
+      <Grid container style={{ border: '1px solid grey', padding: '15px', marginBottom: '10px' }} gap={2}>
         <Grid item style={{ maxWidth: '200px', maxHeight: '200px' }}>
           <CardMedia component="img" width="100%" height="auto" image={cardImage} alt={hotel.name} />
+        </Grid>
+
+        <Grid item>
+          <Grid container justifyContent="space-between" gap={1} alignItems="center">
+            <Grid item>
+              <Typography gutterBottom variant="h5" align="center">
+                {hotel.name}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Rating name="read-only" value={hotel.star} precision={0.5} readOnly size="small" />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </>
