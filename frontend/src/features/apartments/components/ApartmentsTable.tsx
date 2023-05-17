da4137d4786167fd2824b8c470380522cba93ea4
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-17 11:18:57 +0600

    #138 - в ApartmentsTable.tsx кнопочку изменить скрыл от глаз пользователя, кнопочка изменить апартамент доступна только админу и владецельцу апартамента

--- frontend/src/features/apartments/components/ApartmentsTable.tsx
+++ frontend/src/features/apartments/components/ApartmentsTable.tsx
@@ -14,6 +14,7 @@ import { useTranslation } from 'react-i18next';
 import { selectApartments } from '../apartmentSlice';
 import { fetchApartments } from '../apartmentThunks';
 import EditIcon from '@mui/icons-material/Edit';
+import { selectUser } from '../../users/usersSlice';
 
 const Link = styled(NavLink)({
   color: 'inherit',
@@ -29,10 +30,11 @@ interface Props {
 
 const ApartmentsTable: React.FC<Props> = ({ hotel }) => {
   const dispatch = useAppDispatch();
-  const apartments = useAppSelector(selectApartments);
-  const { t } = useTranslation();
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const { id } = useParams();
+  const user = useAppSelector(selectUser);
+  const apartments = useAppSelector(selectApartments);
 
   useEffect(() => {
     dispatch(fetchApartments({ hotelId: hotel._id }));
@@ -61,12 +63,14 @@ const ApartmentsTable: React.FC<Props> = ({ hotel }) => {
             <TableRow key={data._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row" sx={{ textTransform: 'capitalize' }}>
                 <Link to={'/' + hotel.name + '/' + hotel._id + '/apartment/' + data._id}>{data.roomTypeId.name}</Link>
-                <IconButton
-                  sx={{ color: 'grey', '&:hover': { color: 'blue' }, ml: 2 }}
-                  onClick={() => navigate('/hotels/' + id + '/editApartment/' + data._id)}
-                >
-                  <EditIcon />
-                </IconButton>
+                {(user?.role === 'admin' || user?._id === hotel.userId) && (
+                  <IconButton
+                    sx={{ color: 'grey', '&:hover': { color: 'blue' }, ml: 2 }}
+                    onClick={() => navigate('/hotels/' + id + '/editApartment/' + data._id)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                )}
               </TableCell>
               <TableCell align="right">{data.price.usd + ' - ' + data.price.kgs}</TableCell>
             </TableRow>
