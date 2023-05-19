1ceceeecf81382a4dce1feeddf7b27b6d6736347
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-19 17:34:29 +0600

    #135 - навесил hover на карточку MainPageCard.tsx

--- frontend/src/components/UI/BlocksOnMainPage/MainPageCard.tsx
+++ frontend/src/components/UI/BlocksOnMainPage/MainPageCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea, Grid, Typography } from '@mui/material';
+import { CardActionArea, Grid, Typography, Box } from '@mui/material';
 import { BlockOnMainPage } from '../../../types';
 import { useNavigate } from 'react-router-dom';
 
@@ -23,26 +23,37 @@ const MainPageCard: React.FC<Props> = ({ item, city, type }) => {
 
   return (
     <Grid item xs={12} sm={6} md={3}>
-      <Card style={{ maxWidth: '350px', maxHeight: 'auto' }} onClick={() => onClickCard(item.lang)}>
-        <CardActionArea>
-          <CardMedia component="img" width="100%" height="200px" image={item.link} alt={item.link} />
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{
-              position: 'absolute',
-              top: 0,
-              padding: '8px',
-              background: 'rgba(0, 0, 0, 0.5)',
-              color: '#fff',
-              width: '100%',
-              textTransform: 'capitalize',
-            }}
-          >
-            {item.name}
-          </Typography>
-        </CardActionArea>
-      </Card>
+      <Box
+        sx={{
+          '&:hover': {
+            '& div': {
+              background: 'rgba(184, 233, 134, 0.8)', // Light pastel green with transparency on hover
+            },
+          },
+        }}
+        onClick={() => onClickCard(item.lang)}
+      >
+        <Card style={{ maxWidth: '350px', maxHeight: 'auto' }}>
+          <CardActionArea>
+            <CardMedia component="img" width="100%" height="200px" image={item.link} alt={item.link} />
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{
+                position: 'absolute',
+                top: 0,
+                padding: '8px',
+                background: 'rgba(0, 0, 0, 0.5)',
+                color: '#fff',
+                width: '100%',
+                textTransform: 'capitalize',
+              }}
+            >
+              {item.name}
+            </Typography>
+          </CardActionArea>
+        </Card>
+      </Box>
     </Grid>
   );
 };
