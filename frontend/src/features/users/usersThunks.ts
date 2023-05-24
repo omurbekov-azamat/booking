bef884a6921213b77b2533fef209c371b2631394
Artem Markelov <artem77timoha77@gmail.com>
2023-05-24 16:21:12 +0600

    #154 - Сделал запрос в санках юзера для регистрации гугл.

--- frontend/src/features/users/usersThunks.ts
+++ frontend/src/features/users/usersThunks.ts
@@ -101,3 +101,18 @@ export const reAuthorization = createAsyncThunk<User>('user/reAuthorization', as
     throw new Error();
   }
 });
+
+export const googleLogin = createAsyncThunk<User, string, { rejectValue: GlobalError }>(
+  'users/googleLogin',
+  async (credential, { rejectWithValue }) => {
+    try {
+      const response = await axiosApi.post<RegisterResponse>('/users/google', { credential });
+      return response.data.user;
+    } catch (e) {
+      if (isAxiosError(e) && e.response && e.response.status === 400) {
+        return rejectWithValue(e.response.data as GlobalError);
+      }
+      throw e;
+    }
+  },
+);
