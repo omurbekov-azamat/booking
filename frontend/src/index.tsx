0d42a91365155eb5b1b49fe043b421aa997b0fe6
Artem Markelov <artem77timoha77@gmail.com>
2023-05-24 15:02:21 +0600

    #154 - Добавил (GoogleOAuthProvider) в (index.tsx).

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,22 +10,26 @@ import { BrowserRouter } from 'react-router-dom';
 import theme from './theme';
 import { addInterceptors } from './axiosApi';
 import { SnackbarProvider } from 'notistack';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from './constants';
 
 addInterceptors(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Suspense fallback={<CircularProgress />}>
-          <ThemeProvider theme={theme}>
-            <SnackbarProvider>
-              <App />
-            </SnackbarProvider>
-          </ThemeProvider>
-        </Suspense>
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Suspense fallback={<CircularProgress />}>
+            <ThemeProvider theme={theme}>
+              <SnackbarProvider>
+                <App />
+              </SnackbarProvider>
+            </ThemeProvider>
+          </Suspense>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </GoogleOAuthProvider>,
 );
