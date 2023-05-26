88b0831f3552ba0de287c6a35bdcecb555ff6af9
Artem Markelov <artem77timoha77@gmail.com>
2023-05-26 23:44:16 +0600

    #156 - Подготовил компонент к дальнейшей работе . Сделал переводы всех текстовых элементов и кнопок.

--- frontend/src/features/users/components/RestorePassword.tsx
+++ frontend/src/features/users/components/RestorePassword.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -6,8 +6,14 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useTranslation } from 'react-i18next';
+import { useAppDispatch } from '../../../app/hooks';
 
 const RestorePassword = () => {
+  const [email, setEmail] = useState('');
+  const [confirmOpen, setConfirmOpen] = useState(false);
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -18,30 +24,33 @@ const RestorePassword = () => {
     setOpen(false);
   };
 
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div>
       <Button variant="outlined" onClick={handleClickOpen}>
-        Restore Password
+        {t('restorePassword')}
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>{t('restorePassword')}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates occasionally.
-          </DialogContentText>
+          <DialogContentText>{t('restorePasswordDesc')}</DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            label="Email Address"
+            label={t('email')}
             type="email"
             fullWidth
             variant="standard"
+            onChange={handleEmail}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button onClick={handleClose}>{t('cancel')}</Button>
+          <Button onClick={handleClose}>{t('send')}</Button>
         </DialogActions>
       </Dialog>
     </div>
