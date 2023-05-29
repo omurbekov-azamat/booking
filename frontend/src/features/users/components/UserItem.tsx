6bb6b1391165425c5b72567aa46c44f8dd317445
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-29 18:25:15 +0600

    #160 - Создал кнопку Изменить роль, принимает id пользователя

--- frontend/src/features/users/components/UserItem.tsx
+++ frontend/src/features/users/components/UserItem.tsx
@@ -4,6 +4,9 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Typography } from '@mui/material';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import { useAppSelector } from '../../../app/hooks';
+import { selectUser } from '../usersSlice';
+import { LoadingButton } from '@mui/lab';
 import { User } from '../../../types';
 
 interface Props {
@@ -11,6 +14,12 @@ interface Props {
 }
 
 const UserItem: React.FC<Props> = ({ prop }) => {
+  const user = useAppSelector(selectUser);
+
+  const onClickChangeRole = (id: string) => {
+    console.log(id);
+  };
+
   return (
     <Accordion
       sx={{
@@ -28,10 +37,15 @@ const UserItem: React.FC<Props> = ({ prop }) => {
         <Typography textTransform="capitalize">Фамилия: {prop.lastName}</Typography>
         <Typography>Почта: {prop.email}</Typography>
         <Typography>Номер телефона: {prop.phoneNumber}</Typography>
-        <Typography textTransform="capitalize">Роль: {prop.role}</Typography>
         <Typography>Статус: {prop.status}</Typography>
         <Typography>Баланс: {prop.cashback}</Typography>
         <Typography>Верифицирован: {prop.isVerified ? 'Да' : 'Нет'}</Typography>
+        <Typography textTransform="capitalize">Роль: {prop.role}</Typography>
+        {user?.role === 'admin' && (
+          <LoadingButton color="success" onClick={() => onClickChangeRole(prop._id)}>
+            Изменить роль
+          </LoadingButton>
+        )}
       </AccordionDetails>
     </Accordion>
   );
