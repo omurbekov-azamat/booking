a1129efc7eacb53968463eafd9d367e488c6e917
Kwon Valerii <wellko24@gmail.com>
2023-06-06 17:25:01 +0600

    #173 изменил компонент страницы верификации(отображение текста в модальном окне)

--- frontend/src/components/UI/VerifyPage/VerifyPage.tsx
+++ frontend/src/components/UI/VerifyPage/VerifyPage.tsx
@@ -7,12 +7,14 @@ import { sendMail } from '../../../features/users/usersThunks';
 import Collapse from '@mui/material/Collapse';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { useTranslation } from 'react-i18next';
+import ModalCover from '../ModalCover/ModalCover';
 
 const VerifyPage = () => {
   const { t } = useTranslation();
   const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(true);
   const onButtonClick = async () => {
     await dispatch(sendMail());
     setActive(true);
@@ -21,24 +23,23 @@ const VerifyPage = () => {
     <Container>
       {user && !user.isVerified ? (
         <>
-          <Typography variant="h3" textAlign="center">
-            {t('verifyHeader')}
-          </Typography>
-          <Typography sx={{ display: 'inline-block' }} variant="body1" textAlign="center">
-            {t('verifyMail')}
-          </Typography>
-          <Button variant="outlined" onClick={onButtonClick} disabled={active}>
-            {t('sendMail')}
-          </Button>
-          <Collapse in={active}>
-            <Alert
-              iconMapping={{
-                success: <CheckCircleOutlineIcon fontSize="inherit" />,
-              }}
-            >
-              {t('sendSuccess')}
-            </Alert>
-          </Collapse>
+          <ModalCover state={open}>
+            <Typography sx={{ display: 'inline-block' }} variant="body1" textAlign="center">
+              {t('verifyHeader')}, {t('verifyMail')}
+            </Typography>
+            <Button variant="outlined" onClick={onButtonClick} disabled={active}>
+              {t('sendMail')}
+            </Button>
+            <Collapse in={active}>
+              <Alert
+                iconMapping={{
+                  success: <CheckCircleOutlineIcon fontSize="inherit" />,
+                }}
+              >
+                {t('sendSuccess')}
+              </Alert>
+            </Collapse>
+          </ModalCover>
         </>
       ) : (
         <NoFoundPage />
