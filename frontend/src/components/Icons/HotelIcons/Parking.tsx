261b5ebeeb6fd30b939723d1c2e48ceac24f8e15
Artem Markelov <artem77timoha77@gmail.com>
2023-06-12 12:58:40 +0600

    #198 - Изменил внешний вид доп услуг в большой карточке отеля .

--- frontend/src/components/Icons/HotelIcons/Parking.tsx
+++ frontend/src/components/Icons/HotelIcons/Parking.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Box, Typography } from '@mui/material';
 import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import PetsIcon from '@mui/icons-material/Pets';
 
 interface Props {
   parking: boolean;
@@ -14,20 +15,17 @@ const Parking: React.FC<Props> = ({ parking }) => {
       {!parking ? (
         ''
       ) : (
-        <Box bgcolor="orange" borderRadius={3} paddingX={1} marginX={1} display="inline-block">
-          <Typography
-            variant="body1"
-            display="inline-block"
-            sx={{ borderRadius: '50%', border: 'medium', borderColor: '#FFF' }}
-          >
-            <LocalParkingIcon />
-          </Typography>
-          <Typography
-            variant="body1"
-            component="span"
-            display="inline-block"
-            sx={{ fontWeight: 'bold', paddingLeft: '3px', margin: 'auto' }}
-          >
+        <Box
+          borderRadius={5}
+          marginX={1}
+          padding={1}
+          display="flex"
+          alignItems="center"
+          border={2}
+          borderColor="orange"
+        >
+          <LocalParkingIcon sx={{ color: '#000', fontSize: 24, marginRight: 3, marginLeft: 1 }} />
+          <Typography variant="body1" component="span" sx={{ fontWeight: 'bold', color: '#000' }}>
             {t('parking')}
           </Typography>
         </Box>
