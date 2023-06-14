87f66332b0cda4dad57bd0f45a7563525756d940
Edil Biktimirov <idyabik@gmail.com>
2023-06-14 13:26:17 +0600

    #200 - Добавил возможность использования CommentForm для редактирования комментариев

--- frontend/src/features/comments/components/FormComments.tsx
+++ frontend/src/features/comments/components/FormComments.tsx
@@ -4,24 +4,34 @@ import { selectCreateCommentError, selectLoadingCreateComment } from '../comment
 import { LoadingButton } from '@mui/lab';
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { createComment, fetchComments } from '../commentsThunks';
+import { createComment, fetchComments, updateComment } from '../commentsThunks';
 import { CommentMutation } from '../../../types';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   hotelId?: string;
   isEdit?: boolean;
+  commentId?: string;
+  editedComment?: CommentMutation;
 }
 
-const FormComments: React.FC<Props> = ({ hotelId, isEdit }) => {
+const FormComments: React.FC<Props> = ({ hotelId, isEdit, commentId, editedComment }) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const ErrorCreateComment = useAppSelector(selectCreateCommentError);
   const loadingCreateComment = useAppSelector(selectLoadingCreateComment);
+  const navigate = useNavigate();
 
-  const [state, setState] = useState<CommentMutation>({
-    hotel: hotelId as string,
-    text: '',
-  });
+  const initialState = editedComment
+    ? {
+        ...editedComment,
+      }
+    : {
+        hotel: hotelId as string,
+        text: '',
+      };
+
+  const [state, setState] = useState<CommentMutation>(initialState);
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -30,8 +40,14 @@ const FormComments: React.FC<Props> = ({ hotelId, isEdit }) => {
 
   const submitFormHandler = async (e: React.FormEvent) => {
     e.preventDefault();
-    await dispatch(createComment(state)).unwrap();
-    await dispatch(fetchComments(hotelId as string));
+
+    if (isEdit && commentId) {
+      await dispatch(updateComment({ id: commentId, comment: state })).unwrap();
+      navigate('/hotels/' + hotelId);
+    } else {
+      await dispatch(createComment(state)).unwrap();
+      await dispatch(fetchComments(hotelId as string));
+    }
   };
 
   const getFieldError = (fieldName: string) => {
@@ -53,7 +69,7 @@ const FormComments: React.FC<Props> = ({ hotelId, isEdit }) => {
             name="text"
             rows={4}
             fullWidth={true}
-            label="Write comment..."
+            label={isEdit ? 'Edit comment' : 'Write comment...'}
             id="fullWidth"
             error={Boolean(getFieldError('text'))}
             helperText={getFieldError('text')}
@@ -66,7 +82,7 @@ const FormComments: React.FC<Props> = ({ hotelId, isEdit }) => {
               color="success"
               sx={{ background: '#03C988' }}
             >
-              {t('send')}
+              {isEdit ? t('edit') : t('send')}
             </LoadingButton>
           </Box>
         </CardContent>
