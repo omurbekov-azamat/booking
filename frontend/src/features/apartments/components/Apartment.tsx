f2907147819d382e8209b7c5e094d81c866f9c29
omurbekov-azamat <avgusttin8@gmail.com>
2023-06-15 12:25:42 +0600

    #201 - исправил перевод, добавил изменения в компоненте Apartment.tsx

--- frontend/src/features/apartments/components/Apartment.tsx
+++ frontend/src/features/apartments/components/Apartment.tsx
@@ -44,39 +44,37 @@ const Apartment = () => {
   return (
     <>
       {loadingFetchOneApartment && <Spinner />}
-      <Grid container>
-        <Grid item xs={12} sm={12} md={6} lg={6}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={6} lg={6} order={{ xs: 2, md: 1 }}>
           <Grid container gap={3} flexDirection="column">
             <Typography variant="h5" textTransform="capitalize" fontWeight="bold">
               {i18n.language === 'en' ? apartment?.roomTypeId.name.en : apartment?.roomTypeId.name.ru}
             </Typography>
-            <Grid item>
-              <Grid container direction="row" spacing={1} maxWidth={500}>
-                {apartment?.AC && <CurveIcon icon={<AcUnitIcon />} text="AC" />}
-                {apartment?.balcony && <CurveIcon icon={<BalconyIcon />} text="balcony" />}
-                {apartment?.bath && <CurveIcon icon={<BathtubIcon />} text="bath" />}
-                {apartment?.petFriendly && <CurveIcon icon={<PetsIcon />} text="petFriendly" />}
-                {apartment?.food && <CurveIcon icon={<RestaurantIcon />} text="food" />}
-                {apartment?.tv && <CurveIcon icon={<TvIcon />} text="tv" />}
-                {apartment?.towel && <CurveIcon icon={<DryCleaningIcon />} text="towel" />}
-                {apartment?.wifi && <CurveIcon icon={<WifiIcon />} text="wiFi" />}
-                {apartment?.place && <CurveIcon icon={<TabUnselectedIcon />} text={`${apartment.place} M2`} />}
-              </Grid>
+            <Grid container direction="row" spacing={1} maxWidth={500}>
+              {apartment?.AC && <CurveIcon icon={<AcUnitIcon />} text="AC" />}
+              {apartment?.balcony && <CurveIcon icon={<BalconyIcon />} text="balcony" />}
+              {apartment?.bath && <CurveIcon icon={<BathtubIcon />} text="bath" />}
+              {apartment?.petFriendly && <CurveIcon icon={<PetsIcon />} text="petFriendly" />}
+              {apartment?.food && <CurveIcon icon={<RestaurantIcon />} text="food" />}
+              {apartment?.tv && <CurveIcon icon={<TvIcon />} text="tv" />}
+              {apartment?.towel && <CurveIcon icon={<DryCleaningIcon />} text="towel" />}
+              {apartment?.wifi && <CurveIcon icon={<WifiIcon />} text="wiFi" />}
+              {apartment?.place && <CurveIcon icon={<TabUnselectedIcon />} text={`${apartment.place}  M²`} />}
             </Grid>
-            <Typography gutterBottom component="p">
-              {t('price') + ': ' + (currency === 'kgs' ? apartment?.price.kgs + ' KGS' : apartment?.price.usd + ' USD')}
-            </Typography>
-            <Typography gutterBottom component="p">
+            <Typography textTransform="capitalize" fontWeight="bold">
               {t('place') + ': ' + apartment?.place + ' M²'}
             </Typography>
-            <Typography gutterBottom component="p">
+            <Typography component="p">
+              {t('price') + ': ' + (currency === 'kgs' ? apartment?.price.kgs + ' KGS' : apartment?.price.usd + ' USD')}
+            </Typography>
+            <Typography component="p">
               {i18n.language === 'en' ? apartment?.description.en : apartment?.description.ru}
             </Typography>
           </Grid>
           <Box textAlign="right">
             <Button
               variant="outlined"
-              size={'large'}
+              size="small"
               sx={{ mx: 'auto', my: 3, display: 'block' }}
               onClick={onClickResolveApartment}
             >
@@ -84,7 +82,7 @@ const Apartment = () => {
             </Button>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} alignSelf="center">
+        <Grid item xs={12} sm={12} md={6} lg={6} alignSelf="center" order={{ xs: 1, md: 2 }}>
           {apartment && <ApartmentsGallery apartmentData={apartment} />}
         </Grid>
       </Grid>
