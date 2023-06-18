528e006ed1999a8bd95d732d6325536bc989c199
Kwon Valerii <wellko24@gmail.com>
2023-06-18 21:56:20 +0600

    #199 изменил конфиг для правильной отработки google регистрации

--- backend/config.ts
+++ backend/config.ts
@@ -12,11 +12,11 @@ const config = {
   publicPath: path.join(rootPath, 'public'),
   db: 'mongodb://localhost/booking',
   google: {
-    clientId: process.env.GOOGLE_CLIENT_ID || '',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    clientId: process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID : '',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ? process.env.GOOGLE_CLIENT_SECRET : '',
   },
   mail: 'esdpjs17@gmail.com',
-  site: process.env.WEBSITE_ADDRESS || 'http://localhost:3000',
+  site: process.env.WEBSITE_ADDRESS ? process.env.WEBSITE_ADDRESS : 'http://localhost:3000',
 };
 
 export default config;
