95836e8aa0734b3fb6da5ddfb50517095d6284c5
omurbekov-azamat <avgusttin8@gmail.com>
2023-06-26 12:25:25 +0600

    #231 - добавил открытие модалки при клике на кнопку delete

--- frontend/src/features/roomTypes/components/RoomTypeItem.tsx
+++ frontend/src/features/roomTypes/components/RoomTypeItem.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card, Typography } from '@mui/material';
-import { someStyle } from '../../../styles';
+import Dialog from '@mui/material/Dialog';
+import { Card, DialogActions, DialogContent, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
+import { LoadingButton } from '@mui/lab';
+import { someStyle } from '../../../styles';
 import { IRoomType } from '../../../types';
 
 export interface Props {
@@ -11,6 +13,9 @@ export interface Props {
 
 const RoomTypeItem: React.FC<Props> = ({ item }) => {
   const { t, i18n } = useTranslation();
+
+  const [open, setOpen] = useState(false);
+
   return (
     <Card
       key={item._id}
@@ -30,9 +35,24 @@ const RoomTypeItem: React.FC<Props> = ({ item }) => {
       <Button sx={{ background: '#05BFDB' }} size="small" variant="contained">
         {t('edit')}
       </Button>
-      <Button size="small" variant="contained" sx={{ background: ' #CD1818' }} color="error">
+      <Button
+        size="small"
+        variant="contained"
+        sx={{ background: ' #CD1818' }}
+        color="error"
+        onClick={() => setOpen(true)}
+      >
         {t('delete')}
       </Button>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogContent>
+          <Typography variant="body1">Вы уверены, что хотите удалить выбранный тип комнаты?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>{t('cancel')}</Button>
+          <LoadingButton>{t('continue')}</LoadingButton>
+        </DialogActions>
+      </Dialog>
     </Card>
   );
 };
