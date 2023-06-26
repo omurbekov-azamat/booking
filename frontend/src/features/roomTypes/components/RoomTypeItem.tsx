70861ec506fd2551d84f1c2faf3610004512ce64
omurbekov-azamat <avgusttin8@gmail.com>
2023-06-26 12:19:42 +0600

    #231 - добавил две кнопки edit, delete в компоненте RoomTypeItem.tsx

--- frontend/src/features/roomTypes/components/RoomTypeItem.tsx
+++ frontend/src/features/roomTypes/components/RoomTypeItem.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card, Typography } from '@mui/material';
-import { IRoomType } from '../../../types';
 import { someStyle } from '../../../styles';
+import Button from '@mui/material/Button';
+import { IRoomType } from '../../../types';
 
 export interface Props {
   item: IRoomType;
 }
 
 const RoomTypeItem: React.FC<Props> = ({ item }) => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <Card
       key={item._id}
@@ -18,11 +19,20 @@ const RoomTypeItem: React.FC<Props> = ({ item }) => {
         p: 2,
         maxWidth: '500px',
         boxShadow: someStyle.boxShadow,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
       }}
     >
       <Typography key={item._id} textTransform="capitalize">
         {i18n.language === 'en' ? item.name.en : item.name.ru}
       </Typography>
+      <Button sx={{ background: '#05BFDB' }} size="small" variant="contained">
+        {t('edit')}
+      </Button>
+      <Button size="small" variant="contained" sx={{ background: ' #CD1818' }} color="error">
+        {t('delete')}
+      </Button>
     </Card>
   );
 };
